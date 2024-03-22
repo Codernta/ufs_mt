751cea9138ed1b674bce0d610c06216f0db37f62
codernta <ta007nithin@gmail.com>
2024-03-22 18:20:43 +0530

    follow commit

--- lib/Utils/Widgets/customer_review_card.dart
+++ lib/Utils/Widgets/customer_review_card.dart
@@ -23,14 +23,17 @@ class CustomerReviewCard extends StatelessWidget {
           children: [
             Expanded(
               flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: SvgPicture.asset('assets/images/customer_image.svg',)),
-                  Image.asset('assets/images/comma_icon.png')
-                ],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: SvgPicture.asset('assets/images/customer_image.svg',)),
+                    Image.asset('assets/images/comma_icon.png')
+                  ],
+                ),
               ),
             ),
             Expanded(
@@ -44,36 +47,39 @@ class CustomerReviewCard extends StatelessWidget {
                     Transform(
                         transform: Matrix4.translationValues(-7, 0, 0),
                         child: Image.asset('assets/images/company_name.png')),
-                    SizedBox(height: 10,),
-                    Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC",style: customerCardSubTitleStyle,)
-                    ,SizedBox(height: 20,),
-                    Container(
-                      height: 25,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            flex: 1,
-                            child: VerticalDivider(
-                              width: 4,
-                              color: Color(0xff0950a0),
+                    const SizedBox(height: 10,),
+                    Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC",style: customerCardSubTitleStyle,),
+                    const SizedBox(height: 20,),
+                    Transform(
+                      transform: Matrix4.translationValues(0, 7, 0),
+                      child: Container(
+                        height: 25,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Expanded(
+                              flex: 1,
+                              child: VerticalDivider(
+                                width: 8,
+                                color: Color(0xff0950a0),
+                              ),
                             ),
-                          ),
-                          Expanded(
-                            flex: 10,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: Text('Jerin Thomas',style: customerNameStyle,),
-                                ),
-                                Expanded(flex: 1,
-                                child: Text('CEO & CO-FOUNDER OF ARABTEC'),)
-                              ],
-                            ),
-                          )
-                        ],
+                            Expanded(
+                              flex: 10,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text('Jerin Thomas',style: customerNameStyle,),
+                                  ),
+                                  const Expanded(flex: 1,
+                                  child: Text('CEO & CO-FOUNDER OF ARABTEC'),)
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ],
